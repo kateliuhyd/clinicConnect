e61f8649e0a7dce95b0084b6b24b7bc303372300
Arav Dharmesh Panchmatia <136196166+BinaryAP@users.noreply.github.com>
2025-08-05 21:06:43 -0700

    Updated User.sql

--- src/main/resources/static/sql/User.sql
+++ src/main/resources/static/sql/User.sql
@@ -1,7 +1,7 @@
-INSERT INTO ClinicDB.`User` (user_id,password,user_type) VALUES
-	 ('doc101','pass001','doctor'),
-	 ('doc102','pass002','doctor'),
-	 ('doc103','pass003','doctor'),
-	 ('doc104','pass004','doctor'),
-	 ('pat001','pw001','patient'),
-	 ('pat002','pw002','patient');
+INSERT INTO ClinicDB.User (user_id,password,user_type, addr_id) VALUES
+	 ('doc101','pass001','doctor','addr1'),
+	 ('doc102','pass002','doctor','addr2'),
+	 ('doc103','pass003','doctor','addr3'),
+	 ('doc104','pass004','doctor','addr4'),
+	 ('pat001','pw001','patient','addr5'),
+	 ('pat002','pw002','patient','addr6');
