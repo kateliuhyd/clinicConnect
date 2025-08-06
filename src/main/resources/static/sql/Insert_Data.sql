2589c7cde10f10494cf449b9e3e4ab178e45bfa4
Arav Panchmatia <aravpanchmatia0@gmail.com>
2025-08-05 20:43:40 -0700

    Indent

--- src/main/resources/static/sql/Insert_Data.sql
+++ src/main/resources/static/sql/Insert_Data.sql
@@ -57,9 +57,9 @@ ALTER TABLE ClinicDB.Appointment
     ADD COLUMN status VARCHAR(20) NOT NULL DEFAULT 'PENDING';
 
 UPDATE ClinicDB.Appointment
-SET status = 'PENDING';INSERT INTO Prescription
-    (medicine_name, prescription_date, pid, did)
-VALUES
+SET status = 'PENDING';
+
+INSERT INTO Prescription (medicine_name, prescription_date, pid, did) VALUES
     ('Amoxicillin 500mg', '2025-01-15', 'pat001', 'doc101'),
     ('Ibuprofen 200mg', '2025-06-16', 'pat002', 'doc102'),
     ('Atorvastatin 10mg', '2025-03-17', 'pat001', 'doc103'),
